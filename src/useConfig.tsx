583e675bfcbff33e498e3fa7adb2de50d76b7390
Ignacio Norambuena <ignacio.norambuena@unholster.com>
2023-06-16 11:08:50 -0400

    Using typescript

--- src/useConfig.tsx
+++ src/useConfig.tsx
@@ -1,42 +1,42 @@
-import React, {
-  createContext, useContext, useEffect, useState,
-} from 'react';
+import React, { ReactNode, createContext, useContext, useEffect, useState } from "react"
 
-const ConfigContext = createContext(undefined);
+const ConfigContext = createContext<unknown>({})
 
-export function useConfig() {
-  return useContext(ConfigContext);
+export function useConfig<AdaptedConfig = unknown>() {
+  return useContext<AdaptedConfig | unknown>(ConfigContext)
 }
 
-interface ConfigProviderProps <Config = unknown, AdaptedConfig = Config> {
-  children: ReactNode;
-  configPath: string;
-  adapter?: (config: Config) => AdaptedConfig;
+interface ConfigProviderProps<Config = object, AdaptedConfig = Config> {
+  children: ReactNode
+  configPath: string
+  adapter?: (config: Config) => AdaptedConfig
 }
 
-export function ConfigProvider<Config, AdaptedConfig>({ children, configPath = 'config.json', adapter}: ConfigProviderProps) {
-  const [config, setConfig] = useState<Config | AdaptedConfig | {}>({});
+export function ConfigProvider<Config, AdaptedConfig>({
+  children,
+  configPath = "config.json",
+  adapter,
+}: ConfigProviderProps) {
+  const [config, setConfig] = useState<Config | AdaptedConfig | object>({})
 
   useEffect(() => {
     fetch(configPath, {
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
     })
       .then((r) => r.json())
       .then((json) => {
-        const data = json || {};
+        const data = json || {}
         const adaptedData: AdaptedConfig = adapter ? adapter(data) : data
-        setConfig(adaptedData);
+        setConfig(adaptedData)
       })
       .catch((err) => {
-        setConfig({});
-        console.warn(`Couldn't load config from ${configPath}:\n ${err}`);
-      });
-  }, []);
+        setConfig({})
+        console.warn(`Couldn't load config from ${configPath}:\n ${err}`)
+      })
+  }, [])
 
-  return (
-    <ConfigContext.Provider value={config}>{children}</ConfigContext.Provider>
-  );
+  return <ConfigContext.Provider value={config}>{children}</ConfigContext.Provider>
 }
